6da74c82fe8ad965edb4b6acc4bae6cdea7034ec
tbendev <tb.devweb@gmail.com>
2026-08-11 16:18:22 +0200

    Ajout de la page galerie et annotation HTML

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+  new Route("/", "Accueil", "/pages/home.html"),
+  new Route("/galerie", "La galerie", "/pages/galerie.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websitename = "Quai Antique";

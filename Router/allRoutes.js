e4f913a39c26b4fcf2ee7f7d57a551db087ef0de
tbendev <tb.devweb@gmail.com>
2026-08-12 11:37:40 +0200

    feat: gestion de l'affichage des reservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,7 +7,12 @@ export const allRoutes = [
   new Route("/signin", "Connexion", "/pages/auth/signin.html"),
   new Route("/signup", "Inscription", "/pages/auth/signup.html"),
   new Route("/account", "Mon compte", "/pages/auth/account.html"),
-  new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
+  new Route(
+    "/editPassword",
+    "Changement de mot de passe",
+    "/pages/auth/editPassword.html",
+  ),
+  new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename

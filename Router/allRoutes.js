06827161776c109714aa7987e9dec12484d44549
tbendev <tb.devweb@gmail.com>
2026-08-12 17:33:46 +0200

    feat: création du formulaire et de la page de réservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -13,6 +13,7 @@ export const allRoutes = [
     "/pages/auth/editPassword.html",
   ),
   new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+  new Route("/reserver", "Réserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename

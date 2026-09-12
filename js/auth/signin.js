9fcd163f96a6c4bb85a0943b75c3eb75db481a10
tbendev <tb.devweb@gmail.com>
2026-09-12 18:45:03 +0200

    feat: mise en place de la logique d'authentification et gestion des cookies

---
+++ js/auth/signin.js
@@ -0,0 +1,21 @@
+const mailInput = document.getElementById("EmailInput");
+const passwordInput = document.getElementById("PasswordInput");
+const btnSignin = document.getElementById("btnSignin");
+
+btnSignin.addEventListener("click", checkCredentials);
+
+function checkCredentials() {
+  //Ici il faudras appeler l'API pour vérifier les credentials en BDD
+
+  if (mailInput.value === "test@gmail.com" && passwordInput.value === "123") {
+    //Il faudras récupérer le vrai token
+    const token = "alzdaklzcazkndazbhazdazdfazcazcaz";
+    setToken(token);
+    //Placer ce token en cookie
+
+    window.location.replace("/");
+  } else {
+    mailInput.classList.add("is-invalid");
+    passwordInput.classList.add("is-invalid");
+  }
+}
